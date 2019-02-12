015e03aa281f71638be7ace3e8d077c64cb15f4c
Tolga Dur <durtolga1@gmail.com>
2019-02-12 17:00:53 +0000

    Update mentimeter-bot.js

--- mentimeter-bot.js
+++ mentimeter-bot.js
@@ -1,30 +1,15 @@
 count = 1
-while(count < 100){
+while(count <= 10){
   document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
-  document.getElementById('mcq-2').click();
+  document.getElementById('mcq-1').click();
 
   var node_list = document.getElementsByTagName('button');
   for (var i = 0; i < node_list.length; i++) {
     var node = node_list[i];
 
     if (node.getAttribute('type') == 'submit') {
-      // do something here with a <input type="text" .../>
-      // we alert its value here
       node.click();
     }
   }
-  location.reload();
-
-  async function demo() {
-    console.log('Taking a break...');
-    await sleep(2000);
-    console.log('Two seconds later');
-  }
-  console.log("printed" + String(count));
   count++;
-}
-
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
 }
