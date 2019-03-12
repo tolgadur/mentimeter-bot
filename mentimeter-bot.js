54ea7a851d33cc8a4f2adc4fbae0aac4abe1b68b
Tolga Dur <durtolga1@gmail.com>
2019-03-12 15:33:33 +0000

    Update mentimeter-bot.js

--- mentimeter-bot.js
+++ mentimeter-bot.js
@@ -1,5 +1,6 @@
 whichOne = 'mcq-1'   
 howMany = 10
+
 count = 1
 while(count <= howMany){
   document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
