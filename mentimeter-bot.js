49efa012f50f38479893da06302e15dadff7e102
Tolga Dur <durtolga1@gmail.com>
2019-03-12 15:32:50 +0000

    Update mentimeter-bot.js

--- mentimeter-bot.js
+++ mentimeter-bot.js
@@ -1,7 +1,9 @@
+whichOne = 'mcq-1'   
+howMany = 10
 count = 1
-while(count <= 10){
+while(count <= howMany){
   document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
-  document.getElementById('mcq-1').click();
+  document.getElementById(whichOne).click();
 
   var node_list = document.getElementsByTagName('button');
   for (var i = 0; i < node_list.length; i++) {
