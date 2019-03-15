2d6f5ae5b3568e76377aa0b548270d50d8f444d4
Tolga Dur <durtolga1@gmail.com>
2019-03-15 17:38:16 +0000

    Revert

--- mentimeter-bot.js
+++ mentimeter-bot.js
@@ -1,10 +1,7 @@
-whichOne = 'mcq-1'   
-howMany = 10
-
 count = 1
-while(count <= howMany){
+while(count <= 10){
   document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
-  document.getElementById(whichOne).click();
+  document.getElementById('mcq-1').click();
 
   var node_list = document.getElementsByTagName('button');
   for (var i = 0; i < node_list.length; i++) {
